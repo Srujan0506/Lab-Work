2e6f314826ead219e9f4b55006a3358d81485fe5
Srujan0506 <srualone0506@gmail.com>
2025-04-04 19:57:57 +0530

    Initial commit

---
+++ first-unique-character-in-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int arr[26] = {0};
+        for (auto a : s)arr[a - 'a']++;
+
+        for (int i = 0; i < s.size(); i++)if (arr[s[i] - 'a'] == 1)return i;
+
+        return -1;
+    }
+};
