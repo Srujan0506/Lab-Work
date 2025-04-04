a5f5bb0f96758080209d13398339fccf009be696
Srujan0506 <srualone0506@gmail.com>
2025-04-04 19:59:27 +0530

    Initial commit

---
+++ Top-k-frequent-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        vector<int> topKFrequent(vector<int>& nums, int k) {
+            unordered_map<int,int> mp;
+    
+            for(auto num : nums) mp[num]++;
+            
+            vector<pair<int,int>> v(mp.begin(),mp.end());
+    
+            sort(v.begin(),v.end(),[](pair<int,int> &a, pair<int,int> &b){return a.second > b.second;});
+    
+            vector<int> ans;
+            for(int i = 0; i<k; i++) ans.push_back(v[i].first);
+    
+            return ans;
+        }
+    };
